39f1d43a59d7ab1001d9820e834889fd8c5d9100
JAEMIN <139306130+jm-jeong@users.noreply.github.com>
2024-08-04 16:18:58 +0900

    Init

---
+++ src/main/java/com/fastcampus/fastcampusprojectboardreview/FastcampusProjectBoardReviewApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusprojectboardreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectBoardReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusProjectBoardReviewApplication.class, args);
+	}
+
+}
