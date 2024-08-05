f8786c85c34aa0755a215b8b2b3f95cbf7f7e988
jaemin <jjm7655@gmail.com>
2024-08-06 01:47:14 +0900

    #7-feat: ArticleController/AuthController Test add

---
+++ src/test/java/com/fastcampus/fastcampusprojectboardreview/controller/AuthControllerTest.java
@@ -0,0 +1,34 @@
+package com.fastcampus.fastcampusprojectboardreview.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fastcampus.fastcampusprojectboardreview.config.SecurityConfig;
+
+@DisplayName("View Controller 인증")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+	private final MockMvc mockMvc;
+
+	public AuthControllerTest(@Autowired MockMvc mockMvc) {
+		this.mockMvc = mockMvc;
+	}
+
+	@DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
+	@Test
+	public void givenNothing_whenTryingToLogin_thenReturnLoginView() throws Exception {
+		//when & then
+		mockMvc.perform(get("/login"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+	}
+}
