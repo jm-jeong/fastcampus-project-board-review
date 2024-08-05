1304ffd87352fc4b140f16e4739016559f026c56
jaemin <jjm7655@gmail.com>
2024-08-05 16:52:41 +0900

    #3-feat: AuditingFields 추출

---
+++ src/main/java/com/fastcampus/fastcampusprojectboardreview/domain/AuditingFields.java
@@ -0,0 +1,41 @@
+package com.fastcampus.fastcampusprojectboardreview.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EntityListeners(value = AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditingFields {
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@CreatedBy
+	@Column(nullable = false, updatable = false, length = 100)
+	private String createdBy;
+
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime modifiedAt;
+
+	@LastModifiedBy
+	@Column(nullable = false, length = 100)
+	private String modifiedBy;
+}
