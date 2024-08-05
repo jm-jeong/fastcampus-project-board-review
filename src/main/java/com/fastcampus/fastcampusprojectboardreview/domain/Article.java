71cd32cc9baab60ff32c86e87754b70f1adac6d4
jaemin <jjm7655@gmail.com>
2024-08-05 18:26:38 +0900

    #5-feat: Springboot3 javax -> jakarta

--- src/main/java/com/fastcampus/fastcampusprojectboardreview/domain/Article.java
+++ src/main/java/com/fastcampus/fastcampusprojectboardreview/domain/Article.java
@@ -4,16 +4,16 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
 
 import lombok.AccessLevel;
 import lombok.Getter;
--- src/main/java/com/fastcampus/fastcampusprojectboardreview/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/fastcampusprojectboardreview/domain/ArticleComment.java
@@ -2,15 +2,14 @@ package com.fastcampus.fastcampusprojectboardreview.domain;
 
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
