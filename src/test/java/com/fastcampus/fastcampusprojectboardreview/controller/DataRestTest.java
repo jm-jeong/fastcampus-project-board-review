ca5c62e9ee0cb1fced0a55344c7e1cb098358836
jaemin <jjm7655@gmail.com>
2024-08-05 17:38:35 +0900

    #5-feat: Data Rest Test 제외

--- src/test/java/com/fastcampus/fastcampusprojectboardreview/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/fastcampusprojectboardreview/controller/DataRestTest.java
@@ -3,6 +3,7 @@ package com.fastcampus.fastcampusprojectboardreview.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Disabled
 @DisplayName("Data REST - API 테스트")
 @AutoConfigureMockMvc
 @SpringBootTest
