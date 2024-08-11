85f01a7d441370aaee06311c4c0620490c59d5fd
jaemin <jjm7655@gmail.com>
2024-08-11 21:23:43 +0900

    #9-feat:페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 `Page` 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/com/fastcampus/fastcampusprojectboardreview/controller/ArticleController.java
+++ src/main/java/com/fastcampus/fastcampusprojectboardreview/controller/ArticleController.java
@@ -2,26 +2,57 @@ package com.fastcampus.fastcampusprojectboardreview.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fastcampus.fastcampusprojectboardreview.domain.type.SearchType;
+import com.fastcampus.fastcampusprojectboardreview.dto.ArticleWithCommentsDto;
+import com.fastcampus.fastcampusprojectboardreview.dto.response.ArticleResponse;
+import com.fastcampus.fastcampusprojectboardreview.dto.response.ArticleWithCommentsResponse;
+import com.fastcampus.fastcampusprojectboardreview.service.ArticleService;
+import com.fastcampus.fastcampusprojectboardreview.service.PagenationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+	private final ArticleService articleService;
+	private final PagenationService pagenationService;
 
 	@GetMapping
-	public String articles(ModelMap map) {
-		map.addAttribute("articles", List.of());
+	public String articles(
+		@RequestParam(required = false) SearchType searchType,
+		@RequestParam(required = false) String searchValue,
+		@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+		ModelMap map
+	) {
+		Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable)
+			.map(ArticleResponse::from);
+		List<Integer> barNumbers = pagenationService.getPaginationBarNumbers(pageable.getPageNumber(),
+			articles.getTotalPages());
+
+		map.addAttribute("articles", articles);
+		map.addAttribute("paginationBarNumbers", barNumbers);
+		
 		return "articles/index";
 	}
 
 	@GetMapping("/{articleId}")
 	public String article(@PathVariable("articleId") Long articleId, ModelMap map) {
-		map.addAttribute("article", "article");//TODO article 데이터 넣어야함
-		map.addAttribute("articleComments", List.of());
+		ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+
+		map.addAttribute("article", article);
+		map.addAttribute("articleComments", article.articleCommentsResponse());
 
 		return "articles/detail";
 	}
